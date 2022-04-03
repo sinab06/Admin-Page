8cf33254a895437d643115101d8741a1aea9ae88
sinab06 <sabhishek647@gmail.com>
2022-04-03 16:46:43 +0530

    Updated Pagination Page

--- admin-ui/src/components/Dashboard.jsx
+++ admin-ui/src/components/Dashboard.jsx
@@ -3,12 +3,15 @@ import './Dashboard.css';
 import { BsSearch } from 'react-icons/bs';
 import { FaEdit } from 'react-icons/fa';
 import { MdDelete } from 'react-icons/md';
+import { Pagination } from "./Pagination";
+
 
 export const Dashboard = () => {
 
     const url = "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
     const [user, setUser] = useState([]);
     const [isChecked, setIsChecked] = useState([]);
+    const [currentUsers, setCurrentUsers] = useState([]);
 
     function getData() {
         fetch(url).then((data) => {
@@ -20,7 +23,7 @@ export const Dashboard = () => {
     }
 
     useEffect(() => {
-       getData()
+        getData()
     }, [])
     // console.log(user) ;
 
@@ -31,65 +34,68 @@ export const Dashboard = () => {
 
     const handleCheckbox = (id) => {
         console.log(isChecked)
-        if(isChecked.includes(id)){
+        if (isChecked.includes(id)) {
             const filteredArray = isChecked.filter(eId => eId !== id)
             setIsChecked(filteredArray)
-          } else {
+        } else {
             setIsChecked([...isChecked, id])
-          }
-        
+        }
     }
 
     return (
-            <div id="body">
+        <div id="body">
+            <div>
+                <h1>
+                    GEEKTRUST Admin Dashboard
+                </h1>
+            </div>
+
+
+            <div id="input_search">
                 <div>
-                    <h1>
-                        GEEKTRUST Admin Dashboard
-                    </h1>
+                    <input type="text" placeholder="Search" id="search" />
                 </div>
 
-
-                <div id="input_search">
-                    <div>
-                        <input type="text" placeholder="Search" id="search" />
-                    </div>
-
-                    <div>
-                        <BsSearch id="search_icon" />
-                    </div>
-                </div>
-
-
                 <div>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th> Employee id</th>
-                                <th> Name </th>
-                                <th> Email </th>
-                                <th> Role </th>
-                                <th> Action </th>
-                                <th><input type="checkbox" value="checked" /></th>
-                            </tr>
-
-                            {
-                                user.map((e) =>
-                                    <tr key={e.id} className={isChecked.includes(e.id) ? "checked" : "unchecked"}>
-                                        <td>#919{e.id}</td>
-                                        <td>{e.name}</td>
-                                        <td>{e.email}</td>
-                                        <td>{e.role}</td>
-                                        <td>
-                                            <span className="action_alignment"> <FaEdit /> </span>
-                                            <span className="action_alignment" onClick={() => handleDelete(e.id)}> <MdDelete size={20} /> </span>
-                                        </td>
-                                        <td><input type="checkbox" value="checked" onChange={() => handleCheckbox(e.id)}/></td>
-                                    </tr>
-                                )
-                            }
-                        </tbody>
-                    </table>
+                    <BsSearch id="search_icon" />
                 </div>
             </div>
-        )
-    }
+
+
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th> Employee id</th>
+                            <th> Name </th>
+                            <th> Email </th>
+                            <th> Role </th>
+                            <th> Action </th>
+                            <th><input type="checkbox" value="checked" /></th>
+                        </tr>
+
+                        {
+                            currentUsers.map((e) =>
+                                <tr key={e.id} className={isChecked.includes(e.id) ? "checked" : "unchecked"}>
+                                    <td>#919{e.id}</td>
+                                    <td>{e.name}</td>
+                                    <td>{e.email}</td>
+                                    <td>{e.role}</td>
+                                    <td>
+                                        <span className="action_alignment"> <FaEdit /> </span>
+                                        <span className="action_alignment" onClick={() => handleDelete(e.id)}> <MdDelete size={20} /> </span>
+                                    </td>
+                                    <td><input type="checkbox" value="checked" onChange={() => handleCheckbox(e.id)} /></td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
+
+            <div id="pagination">
+                <Pagination users={user} setCurrentUsers={setCurrentUsers} />
+            </div>
+        </div>
+    )
+}
--- admin-ui/src/components/Pagination.jsx
+++ admin-ui/src/components/Pagination.jsx
@@ -1,2 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Pagnation.css"
 
+export const Pagination = ({ users, setCurrentUsers }) => {
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const [itemsPerPage] = useState(10)
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+    const pageNumbers = []
+    for (let i = 1; i <= Math.ceil(users.length / itemsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    const setPage = (pageNum) => {
+        let newPage = pageNum;
+        if (pageNum < 1) {
+            newPage = 1
+        } else if (pageNum > pageNumbers[pageNumbers.length - 1]) {
+            newPage = pageNumbers[pageNumbers.length - 1]
+        }
+        setCurrentPage(newPage)
+    }
+
+    useEffect(() => {
+        const currentUsers = users.slice(indexOfFirstItem, indexOfLastItem);
+        setCurrentUsers(currentUsers);
+    })
+
+
+    return (
+        <div>
+            <span style={{ padding: '10px', backgroundColor: 'white', color: 'blue' }} onClick={() => setPage(1)}> First </span>
+
+            <span style={{ padding: '10px', backgroundColor: 'white', color: 'blue' }} onClick={() => setPage(currentPage - 1)}> Prev </span>
+
+            {pageNumbers.map(pageNumber => (<span style={currentPage === pageNumber ? { padding: '10px', backgroundColor: 'yellow', color: 'black' } : { padding: '10px', backgroundColor: 'white', color: 'blue' }} onClick={() => setPage(pageNumber)}>{pageNumber}</span>))}
+
+            <span style={{ padding: '10px', backgroundColor: 'white', color: 'blue' }} onClick={() => setPage(currentPage + 1)}> Next </span>
+
+            <span style={{ padding: '10px', backgroundColor: 'white', color: 'blue' }} onClick={() => setPage(pageNumbers[pageNumbers.length - 1])}> Last </span>
+
+        </div>
+    )
+}
